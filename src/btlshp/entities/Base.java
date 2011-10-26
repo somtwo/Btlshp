b8ee37bc3dfc62f998bb537f9dd2b8b76c358542
Nick <t0pth4t@yahoo.com>
2011-10-26 14:20:20 -0500

    Testing commit

--- src/btlshp/entities/Base.java
+++ src/btlshp/entities/Base.java
@@ -2,7 +2,7 @@ package btlshp.entities;
 
 public class Base extends Construct {
 	/**
-	* Determains if this base is in condition to conduct repairs
+	* Determines if this base is in condition to conduct repairs
 	* @return
 	*/
 	boolean canRepair() {
