79d7b5ff3cf9759da37253355a33f67c14939190
Nick <t0pth4t@yahoo.com>
2011-11-01 20:16:51 -0500

    new Turn classes

---
+++ src/btlshp/turns/loadGameState.java
@@ -0,0 +1,23 @@
+package btlshp.turns;
+
+public class loadGameState extends Turn {
+
+	@Override
+	void executeTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void sendTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	boolean wasSuccessful() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
