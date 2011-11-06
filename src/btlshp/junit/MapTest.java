ba94b0892d68ae8b6afdbc41c195042f5edb84f2
zacharynanfelt <zacharyknanfelt@gmail.com>
2011-11-06 01:48:30 -0500

    added some more to mapTest.java

--- src/btlshp/junit/MapTest.java
+++ src/btlshp/junit/MapTest.java
@@ -1,48 +1,64 @@
 package btlshp.junit;
 
-import static org.junit.Assert.*;
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import btlshp.entities.Map;
-import MapTestStubs.*;
+import btlshp.entities.MapNode;
+import btlshp.entities.Player;
 
 public class MapTest extends TestCase{
-	Player testPlayer1 = new Player();
-	Player testPlayer2 = new Player();
+	MapNode   nodes [][];
+	Player leftPlayer = new Player(); 
+	Player rightPlayer = new Player();
+
 	
 	@Test
-	
-
 	public void testMap() {
-		//Map myMap =  Map(testPlayer1, testPlayer2);   #####CANT FIGURE OUT WHY IT DOESNT WORK#######
-//		Map(Player playerOne, Player playerTwo);
+//		######################################################################################################
+		//Map myMap = new Map(leftPlayer, rightPlayer);   //############CANT FIGURE OUT HOW TO MAKE THIS LINE WORK###############
+//		######################################################################################################
 //		pass if map is 30X30, fail if not
-//		pass if all boats are accounted for, fail if not
-//		pass if all boats are attached to starting dock, fail if not
-//		pass if all reefs are accounted for, and in correct location, fail if not
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 30; j++) {
+				// TODO implement empty node check
+				//assertEquals(expected, MapNode[i][j]);
+			}
+		}
 //		pass if docks are in right location, fail if not
+		
+//		pass if all boats are accounted for, fail if not
+		
+//		pass if all boats are attached to starting dock, fail if not
+		
+//		pass if all reefs are accounted for, and in correct location, fail if not
+
 //		pass if no mines are on playing field, fail if not
+		for (int i = 10; i < 20; i++) {
+			// TODO implement the check player1 base
+			//assertEquals((leftPlayer's base node), MapNode[i][0]);
+		}
+		for (int i = 10; i < 20; i++) {
+			// TODO implement the check player1 base
+			//assertEquals((rightPlayer's base node), MapNode[i][29]);
+		}
 	}
 	
 		//this test case tests both Map() and StoredMap() together...
 	public void testStoreAndLoadMap() {
-		// compare block by block the two representations iif they are both the same -> pass, otherwise fail
+		// compare block by block the two representations if they are both the same -> pass, otherwise fail
 	}
 	
-	public void testAddShip() {
+	public void testAddShipAndRemoveShip() {
 //		for each type of ship:  
 //			add it in a random location, and check with the map block to make sure that the boat is represented.  
-//			pass if represented by correct type of block, fail if not
-	}
-	
-	public void testRemoveShip()  {
-		
+//				pass if represented by correct type of block, fail if not
+//			then remove the ship
+//				pass if the ship is removed from the board, fail if not
 	}
 	
 	public void updateFrame() {
-//		Pass if ship can see opponent’s ship within rador for all four corners of rador, fail if not
+//		Pass if ship can see opponent’s ship within radar for all four corners of radar, fail if not
 //		Pass if submarine can see mine within range, fail if not
 //		pass if the visibility range is correct for all ships, fail if not
 //		pass if all ships are accounted for, fail if not
@@ -52,7 +68,7 @@ public class MapTest extends TestCase{
 	
 	public void testGetMapNode() {
 //		for each type of MapBlock: 
-//			pass if mapblock correctly represents object, fail if object doesnt exist
+//			pass if mapblock correctly represents object, fail if object doesn't exist
 	}
 	
 	public void testCanMove() {
@@ -89,24 +105,28 @@ public class MapTest extends TestCase{
 	}
 	
 	public void testPlaceMine() {
-//		passif mine can be placed into empty place and is placed, fail if not
+//		pass if mine can be placed into empty place and is placed, fail if not
 //		fail if mine can be placed into an occupied space, pass if not
 //		false if sub can pick up mine placed by opponent, fail if not
 	}
+	
 	public void testPickupMine() {
 //		pass if there is a mine, and it is picked up in range, fail if cannot be picked up by either team 
 //		fail if a ‘mine’’ can be picked up from a mapblock where there is no mine, pass if not
 //		fail if a ship other than the sub can pick up a mine, pass if only the sub can
 //		fail if the sub can pick up a mine out of range, pass if not
 	}
+	
 	public void testFireTorpedo() {
 //		true if a torpedo is fired to desired location, false if not
 //		fail if a sub can fire a torpedo, pass if not
 //		fail if a torpedo can be fired anywhere other than straight, pass if not
 	}
+	
 	public void testFireGun() {
 //		pass if ship can fire gun at desired location, fail if not
 //		fail if a sub can fire a gun, pass if not
 //		fail if ship can fire a gun outside of range, pass if not
 	}
+	
 }
