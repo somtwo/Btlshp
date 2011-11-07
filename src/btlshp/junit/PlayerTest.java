fab7980b3b0991813b9837a9e7ef9fb3c2d89438
gerohawj <gerohawj@gmail.com>
2011-11-07 17:44:14 -0600

    Found out how to create ship

--- src/btlshp/junit/PlayerTest.java
+++ src/btlshp/junit/PlayerTest.java
@@ -3,17 +3,36 @@ package btlshp.junit;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import PlayerTestStubs.Ship;
 import btlshp.entities.Player;
 
 public class PlayerTest {
-	public void testPlayer() {
+	Ship s = new Ship();
+	public void testAddShip() {
 		Player myPlayer = new Player();
-		// Assure default of shipCount is 8
-		assertTrue(8 == myPlayer.shipCount());
-		assertFalse(0 == myPlayer.shipCount());
-		
-		
+		// testing addShip
+		myPlayer.addShip(s)
+		;
 		
 	}
 
+	public void testRemoveShip() {
+		//TODO
+	}
+	
+	public void testShipCount() {
+		//TODO
+	}
+	
+	public void testNumberOfMines() {
+		Player myPlayer = new Player();
+		// starting value of mines
+		assertEquals(myPlayer.numberOfMines(), 10);
+		assertFalse(9 == myPlayer.numberOfMines());
+		assertFalse(11 == myPlayer.numberOfMines());
+		
+		myPlayer.addMine();
+		assertEquals(myPlayer.numberOfMines(), 11);
+		
+	}
 }
