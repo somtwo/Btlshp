71adddf624cce62dd14167254ba137d66c223fea
gerohawj <gerohawj@gmail.com>
2011-11-07 12:45:41 -0600

    Finish the player class adding a method addPoint()

--- src/btlshp/entities/Player.java
+++ src/btlshp/entities/Player.java
@@ -1,10 +1,21 @@
 package btlshp.entities;
-
 public class Player {
+	
+	/**
+	 * Instance variables
+	 */
+	private int numberOfShip;
+	private int numberOfMine;
+	private int points;
+	
 	/**
 	 * Default constructor
 	 */
 	public Player() {
+		// Starting value of player
+		numberOfShip = 8;
+		numberOfMine = 10;
+		points = 0;
 	}
 
 	/**
@@ -12,7 +23,7 @@ public class Player {
 	 * @return number of ships player has in play
 	 */
 	public int shipCount() {
-		return 0;
+		return numberOfShip;
 	}
 
 	/**
@@ -20,7 +31,7 @@ public class Player {
 	*@return number of mines player has in play
 	*/
 	public int numberOfMines() {
-		return 0;
+		return numberOfMine;
 	}
 
 	/**
@@ -28,18 +39,31 @@ public class Player {
 	 * @throws IllegalArgumentException if the player has no more mines.
 	 */
 	public void removeMine() {
+		if (numberOfMine == 0)
+		{
+			throw new IllegalArgumentException("No more mines");
+		}
+		numberOfMine = numberOfMine - 1;
 	}
 
 	/**
 	 * Increments the number of mines the player has.
 	 */
 	public void addMine() {
+		numberOfMine = numberOfMine + 1;
+	}
+	
+	/**
+	 * Add points 
+	 */
+	public void addPoints() {
+		points = points + 1;
 	}
 
 	/**
 	 * Calculates the current number of points the player has.
 	 */
 	public int getPoints() {
-		return 0;
+		return points;
 	}
 }
