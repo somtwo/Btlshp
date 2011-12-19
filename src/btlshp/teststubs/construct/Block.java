023759692f3784e4942800714bbebaf511b4c16a
Robert Schaak <raschaak@uwm.edu>
2011-12-19 16:52:35 -0600

    Re-added the deleted files to local...

---
+++ src/btlshp/teststubs/construct/Block.java
@@ -0,0 +1,14 @@
+package btlshp.teststubs.construct;
+
+import btlshp.enums.Weapon;
+
+public class Block {
+	
+	/**
+	 * Checks if this block is destroyed
+	 * @returns True if this block is destroyed
+	 */
+	public void takeHit(Weapon fromWeapon){
+		//Allows for anyBlock to "take a hit"... but many blocks will do nothing by default
+	}
+}
