03f2562891264a23cd3a278359d7eefd4f5da14c
zacharynanfelt <zacharyknanfelt@gmail.com>
2011-12-19 18:12:59 -0600

    rotate180Mode.java

---
+++ src/btlshp/ui/gridmodes/rotate180Mode.java
@@ -0,0 +1,28 @@
+package btlshp.ui.gridmodes;
+
+import btlshp.Btlshp;
+import btlshp.entities.Block;
+import btlshp.entities.Map;
+import btlshp.entities.MapNode;
+import btlshp.entities.Ship;
+import btlshp.enums.Direction;
+import btlshp.turns.TurnFactory;
+import btlshp.ui.GameGrid;
+import btlshp.utility.NodeIterator;
+import btlshp.utility.NodeIteratorAction;
+
+public class rotate180Mode extends GridMode {
+
+	public rotate180Mode(GameGrid gameGrid, Map gameMap) {
+		super(gameGrid, gameMap);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClick(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
